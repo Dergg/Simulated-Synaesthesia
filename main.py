97d43764138cd45c61f1e230a09aebd38f3f49ca
Derg <94351943+Dergg@users.noreply.github.com>
2025-02-13 09:17:20 +0000

    First push
    And so it begins.

---
+++ main.py
@@ -0,0 +1,16 @@
+# IMC Coursework
+
+# Has to involve at least one of the following:
+# 1. Fourier / Inverse Fourier transform
+# 2. Mathematical representations of scales, temparement and/or tuning
+# 3. Computational models of beat, tempo and/or meter
+# 4. Geometric models of harmony and/or voice-leading (e.g. Tonnetz)
+# 5. Sequential models of music data (harmony, melody and/or rhythm)
+# 6. Hierarchical models of music data
+# 7. Neural models of music data
+
+# Current ideas:
+# - "This song sounds like [artist]": deep learning for artist recognition in music
+# - Stem generator: give a music "part" (e.g. "bass", "drums", "lead") and a genre (e.g. "garage", "drum n bass", "rap"),
+#   then generate a random stem for that part given the two parameters. All based on "vibes" and previous experience.
+# 
