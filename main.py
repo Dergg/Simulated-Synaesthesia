38ba7dda7a3f2452d28fb8ac6221d98fcbac534c
Derg <94351943+Dergg@users.noreply.github.com>
2025-02-20 13:29:27 +0000

    Add example MP3 file, spectrogram

--- main.py
+++ main.py
@@ -10,7 +10,32 @@
 # 7. Neural models of music data
 
 # Current ideas:
-# - "This song sounds like [artist]": deep learning for artist recognition in music
-# - Stem generator: give a music "part" (e.g. "bass", "drums", "lead") and a genre (e.g. "garage", "drum n bass", "rap"),
-#   then generate a random stem for that part given the two parameters. All based on "vibes" and previous experience.
-# 
+# - Synysthesia: analysing audio and sscore to produce some visualisation: audiovisual output with colours [2, 3]
+
+import librosa as lib
+import librosa.display
+import pygame
+import pygame.mixer as pgm
+import ffmpeg
+import matplotlib.pyplot as plt
+import numpy as np
+import pydub
+import argparse
+
+parser = argparse.ArgumentParser(prog='PSvis', description='Programmatic Synaesthesia Visualisation algorithm')
+parser.add_argument('infile')
+# parser.add_argument('outfile')
+parser.add_argument('-live', '--live_video', action='store_true') # Optionally shows the MP4 file in a window
+args = parser.parse_args()
+
+aud_path = f'./mp3s/{args.infile}.mp3' # Only works with MP3 files!
+y, sr = lib.load(aud_path, sr=44100) # y = waveform | SR = sample rate
+
+spec = np.abs(librosa.stft(y))
+spec_db = lib.amplitude_to_db(spec, ref=np.max) # Convert the spectrogram to decibels
+
+plt.figure(figsize=(10,5))
+librosa.display.specshow(spec_db, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar(label='dB')
+plt.title('Spectrogram')
+plt.show()
